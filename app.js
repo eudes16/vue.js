9da943cc480940a3558715ce2d3ee58dca58bfcb
Eudes Vicente de Souza Silva <eudes.vss@gmail.com>
2017-03-21 18:49:45 -0300

    Conceitos Básicos - Propriedades computadas

--- app.js
+++ app.js
@@ -1,7 +1,27 @@
 var app = new Vue({
     el: "#app",
     data : {
-        title: "Contas a receber"
+        title: "Contas a pagar",
+        bills: [
+            { date_due: "20/08/2016", name: "Conta de Luz", value: 70.99, done: 1 },
+            { date_due: "21/08/2016", name: "Conta de Água", value: 50.99, done: 0 },
+            { date_due: "22/08/2016", name: "Conta de Telefone", value: 55.99, done: 0 },
+            { date_due: "23/08/2016", name: "Supermercado", value: 625.99, done: 0 },
+            { date_due: "24/08/2016", name: "Cartão de Crédito", value: 1500.99, done: 0 },
+            { date_due: "25/08/2016", name: "Empréstimo", value: 2000.99, done: 0 },
+            { date_due: "26/08/2016", name: "Gasolina", value: 200, done: 0 },
+        ]
+    },
+    computed: {
+        status: function () {
+            var count = 0;
+            for(var i in this.bills) {
+                if (! this.bills[i].done) {
+                    count++;
+                }
+            }
+            return !count ? "Nenhuma conta a pagar" : "Exixtem " + count + " a serem pagas";
+        }
     }
 });
 
